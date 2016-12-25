734110dafa94a6dc84109dda942d499c7823a349
Alexey Zatsepin <alexzatsepin7@gmail.com>
2016-12-25 23:28:12 +0200

    init

---
+++ src/main/java/com/openpolicy/WebConfig.java
@@ -0,0 +1,28 @@
+package com.openpolicy;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "com.openpolicy")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public InternalResourceViewResolver getViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setSuffix(".jsp");
+        resolver.setPrefix("/templates/");
+        resolver.setViewClass(JstlView.class);
+        resolver.setOrder(2);
+        return resolver;
+    }
+
+}
